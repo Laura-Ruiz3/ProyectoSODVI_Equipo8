3b8bc822e4c41e90c8a99f615d9e031c3424513c
Nathan <112341960+NathanPC2004@users.noreply.github.com>
2023-10-21 09:56:00 -0600

    21/10/2023
    Seguna actualizacion de juego en el repositorio
    - Ya se incluyen menu principal  sus respectivos botones

---
+++ Assets/Scrips/Puntuacion.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Puntuacion : MonoBehaviour
+{
+    public TMP_Text puntuacion;
+
+    public float valor_p;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        valor_p += Time.deltaTime;
+
+        puntuacion.text = "Puntuación: " + valor_p.ToString("0");
+    }
+}
